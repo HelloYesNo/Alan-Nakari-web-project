9ab3ab9a06d1175f0ef99c8b46bc6eca78009956
HelloYesNo <38365632+HelloYesNo@users.noreply.github.com>
2021-04-23 19:52:31 +0200

    feat: fetch and display users

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,18 @@
+import { useState, useEffect } from "react";
 import Head from "next/head";
 import Card from "@components/Card";
 
 export default function Home() {
+  const [users, setUsers] = useState(null);
+
+  useEffect(async () => {
+    const response = await fetch("http://localhost:3000/api/users");
+    const data = await response.json();
+    setUsers(data);
+  }, []);
+
+  console.log("users", users);
+
   return (
     <div className="bg-yellow-400">
       <Head>
@@ -10,7 +21,11 @@ export default function Home() {
       </Head>
 
       <main>
-        <Card title="Welcome to my app!" />
+        {users === null
+          ? "loading..."
+          : users.map((user, index) => (
+              <Card key={index} title={user.first_name} />
+            ))}
       </main>
     </div>
   );
